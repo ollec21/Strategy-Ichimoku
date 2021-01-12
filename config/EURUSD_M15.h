881c21618b41d2665598ab1c887385d0fea9c2d1
kenorb <kenorb@users.noreply.github.com>
2021-01-12 00:15:58 +0000

    Optimize Indicator params (pf=0.88; dd=0.52; tt=19)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,11 +1,16 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Ichimoku_Params_M15 : IchimokuParams {
-  Indi_Ichimoku_Params_M15() : IchimokuParams(indi_ichi_defaults, PERIOD_M15) { shift = 0; }
+  Indi_Ichimoku_Params_M15() : IchimokuParams(indi_ichi_defaults, PERIOD_M15) {
+    kijun_sen = 0
+    senkou_span_b = 0
+    shift = 0;
+    tenkan_sen = 0
+  }
 } indi_ichi_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -20,7 +25,7 @@ struct Stg_Ichimoku_Params_M15 : StgParams {
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
