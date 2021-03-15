3eae5da1e7d8f4ec06701b4059c737fe28ff56bc
ollec21 <ollec21@users.noreply.github.com>
2021-03-15 17:36:22 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,10 +6,10 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Ichimoku_Params_M1 : IchimokuParams {
   Indi_Ichimoku_Params_M1() : IchimokuParams(indi_ichi_defaults, PERIOD_M1) {
-    tenkan_sen = 21;
-    kijun_sen = 20;
-    senkou_span_b = 52;
+    kijun_sen = 30
+    senkou_span_b = 42
     shift = 0;
+    tenkan_sen = 31
   }
 } indi_ichi_m1;
 
@@ -20,12 +20,12 @@ struct Stg_Ichimoku_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
